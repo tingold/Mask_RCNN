26e542aa66bfdac84f1225aa077a22a4836c7330
tingold <tingold@boundlessgeo.com>
2018-06-27 19:18:01 -0400

    fixed typo

--- trainBuildings.sh
+++ trainBuildings.sh
@@ -1,3 +1,3 @@
 source activate tensorflow_p36
 export PYTHONPATH=$PYTHONPATH:.
-python samples/buildings/buildings.py && tensorboard --logdir logs &
+python samples/buildings/buildings.py & tensorboard --logdir logs &
